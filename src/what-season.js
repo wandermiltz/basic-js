9db0d157e71b75d74eb9d9b16a791daa23af5a5e
Olga Berezka <olga.berezka.main@gmail.com>
2023-04-12 22:59:44 +0100

    feat: add what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,40 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getSeason(date) {
 
+	if (!date) {
+		return 'Unable to determine the time of year!'
+	}
+
+	if (Object.prototype.toString.call(date) !== '[object Date]' || date.hasOwnProperty('toString')) {
+		throw new Error('Invalid date!')
+	}
+
+	let month = date.getMonth()
+
+	if (month < 2 || month == 11) {
+		return 'winter'
+	}
+	if (month > 7) {
+		return 'autumn'
+	}
+	if (month > 4) {
+		return 'summer'
+	}
+	if (month > 1) {
+		return 'spring'
+	}
+}
 module.exports = {
-  getSeason
+	getSeason
 };
